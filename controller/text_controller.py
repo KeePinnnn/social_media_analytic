0cc185d3a13208a8c34645ceda9c3aff9cd824bd
KeePinnnn <contact@keepin.info>
2019-09-11 03:11:34 +0800

    removed controller file to make the code easier to understand and follow

--- controller/text_controller.py
+++
@@ -1,9 +0,0 @@
-from nlp.text_analysis import text_blob
-
-class text_controller():
-    def create_blob(self, text:str):
-        # remove left and right white spaces
-        sentence = text.lstrip().rstrip()
-        self.blob = text_blob(sentence)
-        return self.blob.get_sentiment()
-        
