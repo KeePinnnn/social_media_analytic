864be856b82ac27da35015c3ba3401a0afafd477
KeePinnnn <contact@keepin.info>
2019-09-17 18:02:18 +0000

    refactor data source code in data.py

--- data_source/data.py
+++ data_source/data.py
@@ -5,6 +5,11 @@ from sklearn import preprocessing
 from sklearn.preprocessing import normalize
 from sklearn.preprocessing import minmax_scale
 from sklearn.model_selection import ShuffleSplit
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
+from sklearn.metrics import accuracy_score
 
 import random
 import os
@@ -125,3 +130,26 @@ class process_data():
 
         self.data.update(self.new_df)
         return self.data
+
+class scikit():
+    def __init__(self, file_path:str):
+        self.file = file_path
+        self.data = pd.read_csv(self.file, engine='python', encoding='utf-8', error_bad_lines=False)
+        self.target = self.data['type']
+        self.data.drop(['type'], inplace=True, axis=1)
+        self.data_train, self.data_test, self.target_train, self.target_test = train_test_split(self.data, self.target)
+
+    def naive_bayes(self):
+        gnb = GaussianNB()
+        pred = gnb.fit(self.data_train, self.target_train).predict(self.data_test)
+        print("Naive-Bayes accuracy : ", accuracy_score(self.target_test, pred, normalize=True))
+
+    def svc(self, shuffle_data:int):
+        svc_model = LinearSVC(random_state=shuffle_data)
+        pred = svc_model.fit(self.data_train, self.target_train).predict(self.data_test)
+        print("LinearSVC accuracy : ", accuracy_score(self.target_test, pred))
+
+    def k_neighbors(self, neighbour:int):
+        neigh = KNeighborsClassifier(n_neighbors=neighbour)
+        pred = neigh.fit(self.data_train, self.target_train).predict(self.data_test)
+        print("KNeighbors accuracy score : ", accuracy_score(self.target_test, pred))
--- nlp/word_test.py
+++ nlp/word_test.py
@@ -1,50 +1,65 @@
-# from gensim.models import KeyedVectors
+from gensim.parsing.preprocessing import remove_stopwords
+from gensim.parsing.preprocessing import strip_punctuation
+from gensim.models import KeyedVectors
 
 from textblob import TextBlob
 import pandas as pd
+import numpy as np
+
+from pattern.en import parse, Sentence, modality
 
 if __name__ == "__main__":
-    # gmodel = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    gmodel = KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin', binary=True)
+    print(dir(gmodel))
+
+    data = pd.read_csv('../data_source/after_drop_data/combined_data_1.csv', engine='python', encoding='utf-8')
+    df = pd.DataFrame(data)
+    content = remove_stopwords(strip_punctuation(df['content'][1]))
+    print(gmodel.score(content.split()))
+    # array = []
+
+    # for word in content.split():
+    #     try:
+    #         vector = gmodel[word]
+    #         array.append(np.average(vector))
+    #     except:
+    #         print(f'word not found {word}')
+    #         pass
+
+    # print(array)
 
     # t_vectors = [gmodel[x] for x in "this is just for testing purpose only".split(' ')]
 
     # print(t_vectors)
 
-    print("start")
-    counter = 1
-    while counter < 5:
-        data = pd.read_csv(f'../data_source/after_drop_data/combined_data_{counter}.csv', engine='python', encoding='utf-8', error_bad_lines=False)
-        df = pd.DataFrame(data=data)
-        # df.dropna(inplace=True)
-        # print("drop done")
-        # print(df.shape)
+    # print("start")
+    # counter = 1
+    # while counter < 5:
+    #     data = pd.read_csv(f'../data_source/after_drop_data/combined_data_{counter}.csv', engine='python', encoding='utf-8', error_bad_lines=False)
+    #     df = pd.DataFrame(data=data)
+    #     # df.dropna(inplace=True)
+    #     # print("drop done")
+    #     # print(df.shape)
 
-        for index, row in df.iterrows():
-            if type(row['content']) is not str:
-                print(row)
-                df.drop(index)
-                print(index)
+    #     print("done reading")
+    #     content = df['content'].tolist()
+    #     modality_list = []
+    #     sub_counter = 0
+    #     for each in content:
+    #         sent = parse(each, lemmata=True)
+    #         sent = Sentence(sent)
+    #         print(f'inside the loop, the counter is f{sub_counter}, file is combined_data_f{counter}')
+    #         modality_list.append(modality(sent))
+    #         sub_counter += 1
 
-        print(f"counter is {counter}")
-            
+    #     print(f"modality list is {len(modality_list)} length")
 
-        content = df['content'].tolist()
-        polarity_list = []
-        subjectivity_list = []
-        for each in content:
-            t = TextBlob(str(each))
-            polarity_list.append(t.sentiment.polarity)
-            subjectivity_list.append(t.sentiment.subjectivity)
+    #     df['modality_score'] = modality_list
+    #     print(f"df is {df.shape}")
+    #     df.to_csv(f'../data_source/after_drop_data/combined_data_{counter}.csv', encoding=False, index=False)
+    #     print(f"written into combined_data_{counter}.csv")
+    #     counter += 1
+    #     # print(counter)
 
-        print(f"polarity list is {len(polarity_list)} length")
-        print(f"subjectivity list is {len(subjectivity_list)} length")
+    # print("done")
 
-        df['polarity_score'] = polarity_list
-        df['subjectivity_score'] = subjectivity_list
-        print(f"df is {df.shape}")
-        df.to_csv(f'../data_source/after_drop_data/combined_data_{counter}.csv', encoding=False, index=False)
-        print(f"written into combined_data_{counter}.csv")
-        counter += 1
-        # print(counter)
-
-    print("done")
