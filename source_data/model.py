19f691bb2bc7fae10b110b179908dfe520dfc10c
KeePinnnn <contact@keepin.info>
2019-10-10 23:45:42 +0800

    successfully run tensorflow and tensorboard!

--- source_data/model.py
+++ source_data/model.py
@@ -82,8 +82,10 @@ binary_label_head = tf.contrib.estimator.binary_classification_head(
 
 estimator = tf.estimator.DNNEstimator(
     head=binary_label_head,
-    hidden_units=[64,10],
-    feature_columns=[text_embedding, title_embedding, author_embedding, url_embedding]
+    hidden_units=[64,32],
+    feature_columns=[text_embedding, title_embedding, author_embedding, url_embedding],
+    batch_norm=True,
+    model_dir="./estimator_64"
 )
 
 features = {
@@ -115,7 +117,7 @@ eval_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn({
                 "url": np.array(test_url).astype(np.str)
                 }, 
                 np.array(test_type).astype(np.int32), 
-                shuffle=False
+                shuffle=True 
                 )
 
 print("start testing")
@@ -124,7 +126,7 @@ print(estimator.evaluate(input_fn=eval_input_fn))
 raw_content_test = [
     "Why won't the Corupt Fake News blame Low IQ Rick Perry for my crimes?! Its NOT MY FAULT that I bribed Ukraine to interfere with the 2020 US election! Dumb as a rock Rick Perry made me do it! Rick Perry says stuff and keeps outsmarting me! Treason!",
     "Another armed man decides it’s time for his entire family to die before killing himself - this time in Massachusetts. A majority of mass shootings in America are instigated by domestic violence.",
-    "Tariffs are forcing China to pay attention to US concerns,   US Secretary of Commerce Wilbur Ross said on Thursday (Oct 10). Mr Ross said the United States would have preferred not to implement tariffs against Chinese goods more than a year ago, which ignited a trade war that slowed global commerce and threatened decades-old systems, but added that it has forced Beijing into action.\nThe trade war has weighed on global growth and roiled financial markets.\n\"We do not love tariffs, in fact we would prefer not to use them, but after years of discussions and no action, tariffs are finally forcing China to pay attention to our concerns,\"\nMr Ross told a business function held by the American Chamber of Commerce in Australia.\n\"We could have had a deal two-and-a-half years ago without going through the whole tit-for-tat on tariffs that we have.\" \nMr Ross is on an official visit to Australia. \nTop US and Chinese trade and economic officials will meet in Washington on Thursday and Friday to try to end a 15-month-old trade war."
+    "Tariffs are forcing China to pay attention to US concerns, US Secretary of Commerce Wilbur Ross said on Thursday (Oct 10). Mr Ross said the United States would have preferred not to implement tariffs against Chinese goods more than a year ago, which ignited a trade war that slowed global commerce and threatened decades-old systems, but added that it has forced Beijing into action.\nThe trade war has weighed on global growth and roiled financial markets.\n\"We do not love tariffs, in fact we would prefer not to use them, but after years of discussions and no action, tariffs are finally forcing China to pay attention to our concerns,\"\nMr Ross told a business function held by the American Chamber of Commerce in Australia.\n\"We could have had a deal two-and-a-half years ago without going through the whole tit-for-tat on tariffs that we have.\" \nMr Ross is on an official visit to Australia. \nTop US and Chinese trade and economic officials will meet in Washington on Thursday and Friday to try to end a 15-month-old trade war."
     ]
 
 raw_title_test = [
@@ -151,7 +153,7 @@ predict_input_fn = tf.compat.v1.estimator.inputs.numpy_input_fn({
                 "author": np.array(raw_author_test).astype(np.str),
                 "url": np.array(raw_url_test).astype(np.str)
                 }, 
-                shuffle=False
+                shuffle=True
                 )
 results = estimator.predict(predict_input_fn)
 for each in results:
